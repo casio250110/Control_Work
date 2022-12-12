f1e86231f42afd8702a302032033b6d6e9b9b483
casio250110 <casio250110@yandex.ru>
2022-12-12 20:48:43 +0300

    Насписали код решения задачи

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите массив данных через запятую(слова и/или целые числа); ");
+string element = Console.ReadLine();
+string[] array1 = element.Split(",");
+string[] array2 = new string[0];
+
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        Array.Resize(ref array2, array2.Length + 1);
+        array2[array2.Length - 1] = array1[i];
+    }
+}
+Console.WriteLine($"{string.Join(", ", array2)}");
